490bf808c96594247899fb388cd34b1911357af1
Aphisit Namracha <aphisiit086757@hotmail.com>
2019-07-01 14:18:52 +0700

    Add test configuration

--- src/test/java/guy/azdo/app/service/CalculateServiceTest.java
+++ src/test/java/guy/azdo/app/service/CalculateServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,6 +17,13 @@ public class CalculateServiceTest {
     @Autowired
     ICalculateService calculateService;
 
+    @TestConfiguration
+    static class CalculateServiceTestContextConfiguration {
+        @Bean
+        public ICalculateService employeeService() {
+            return new CalculateService();
+        }
+    }
 
     @Test
     public void loadAutowired(){
